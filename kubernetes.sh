ff50c051812ba9f3c153ec2e8c64f95693895533
sivakumar9963 <103584198+sivakumar9963@users.noreply.github.com>
2024-02-09 11:39:02 +0530

    Create kubernetes.sh

---
+++ kubernetes.sh
@@ -0,0 +1,11 @@
+apt update -y
+apt install docker.io -y
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --driver=docker --force calico
+minikube status
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo mv kubectl /usr/local/bin/
+sudo chmod +x /usr/local/bin/kubectl
+kubectl cluster-info 
+kubectl get nodes
