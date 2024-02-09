432629ca23decba5070174268e7838e12e75c279
sivakumar9963 <103584198+sivakumar9963@users.noreply.github.com>
2024-02-09 11:49:02 +0530

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,5 +1,6 @@
 apt update -y
 apt install docker.io -y
+usermod -aG docker $USER && newgrp docker
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube start --driver=docker --force calico
